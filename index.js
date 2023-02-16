ff9d33a12572981fb181e3d65c4d7838a30a7288
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2023-02-17 00:20:04 +0545

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-var favicon = require('serve-favicon')
-var path = require('path')
-var cors = require('cors')
-const app = express()
+const express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+var cors = require('cors');
+const app = express();
 
-app.use(express.json())
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.set('view-engine', 'ejs')
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('view-engine', 'ejs');
 
-const linesRouter = require('./routes/lines')
-app.use('/lines', linesRouter)
-app.use(cors())
+const linesRouter = require('./routes/lines');
+app.use('/lines', linesRouter);
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { url: 'simp-api.tk' })
-}) 
+    res.render('index.ejs', { url: 'simp-api.tk' });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server has started")
-})
+    console.log('Server has started');
+});
